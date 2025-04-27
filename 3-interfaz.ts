52e861472e2a42be25a1aeef5f6564b12fec1c5f
Alexis Fleitas Klisch <unbosquense@gmail.com>
2025-04-27 18:52:00 -0300

    3 <- complete

---
+++ 3-interfaz.ts
@@ -0,0 +1,14 @@
+interface Pair <T, U> {
+  first: T,
+  second: U
+}
+
+const reto: Pair<number, string> = {
+  first : 45,
+  second: 'asdaseeee'
+}
+
+const reto2: Pair<boolean, Date> = {
+  first: true,
+  second: new Date
+}
